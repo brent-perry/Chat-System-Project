424becc083f0ed6b48a8ef9d068faf74a80ada33
trevor <50567522+iamtrevorm@users.noreply.github.com>
2021-01-28 10:36:44 -0800

    removed space between parenthesis and curly brackets for functions as per format guidelines

--- util/client.js
+++ util/client.js
@@ -1,30 +1,30 @@
 'use strict';
 
-(function() {
+(function(){
 
     const DEV = true;
 
     let socket = new WebSocket('ws://' + document.location.host);
 
-    socket.onopen = function() {
+    socket.onopen = function(){
       if (DEV) {
         console.log('connection open');
       };
     };
 
-    socket.onclose = function() {
+    socket.onclose = function(){
       if (DEV) {
         console.log('connection closed');
       };
     };
 
-    socket.onerror = function(error) {
+    socket.onerror = function(error){
       if (DEV) {
         console.log(error);
       };
     };
 
-    socket.onmessage = function (message) {
+    socket.onmessage = function (message){
       if (DEV) {
         console.log('recieved message', message);
       };
