690cd1581485928893c9c9f88f96dc3207f27385
JulianForeman <71847719+JulianForeman@users.noreply.github.com>
2021-02-24 09:06:53 -0800

    Packet Builder Functions and Structure (#17)
    * Packet Builder Functions and Structure
    
    Created the functionality to create chat packets and the skeleton for authentication packets.  Also has a packet reader that handles sending the encoded message to all clients on the server.
    
    * Small Fixes
    
    fixing small formatting things
    
    * stuff
    
    more stuff
    
    * stuff
    
    stupid braces
    
    * sdafas
    
    sadfsadf
    
    * Update packet.js

--- client/src/main.js
+++ client/src/main.js
@@ -1,7 +1,9 @@
 'use strict';
 
+import {sendChat} from './socket';
+
 import {test} from './buttonFunctions/buttons';
 
+window.sendChat = sendChat;
+
 test();
-
-
---
+++ client/src/socket.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const {create_chat_message} = require('../../lib/messages/client/chat');
+
+
+    const DEV = true;
+
+    let socket = new WebSocket('ws://' + document.location.host);
+    socket.binaryType = "arraybuffer";
+
+    socket.onopen = function(){
+      if (DEV){
+        console.log('connection open');
+      };
+    };
+
+    socket.onclose = function(){
+      if (DEV){
+        console.log('connection closed');
+      };
+    };
+
+    socket.onerror = function(error){
+      if (DEV){
+        console.log(error);
+      };
+    };
+
+    socket.onmessage = function (message){
+      if (DEV){
+        console.log('recieved message', message);
+      };
+    };
+
+    export function sendChat(username,message){
+      if (typeof username !== "string"){
+        throw new Error('Username is not a string');
+      }
+      else if(typeof message !== "string"){
+        throw new Error('Message is not a string');
+      }
+      else{
+        let packet = create_chat_message(username, message);
+        socket.send(packet.buffer);
+      }
+    }
