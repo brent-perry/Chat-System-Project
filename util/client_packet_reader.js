b8da29292cfe6290aa9128bc5305fbd150b6ab1a
trevor <50567522+iamtrevorm@users.noreply.github.com>
2021-02-25 11:26:43 -0800

    -changing packet to lowercase

--- util/client_packet_reader.js
+++ util/client_packet_reader.js
@@ -4,12 +4,12 @@ const Packet = require('../lib/packet');
 const {CHAT_MESSAGE} = require('../lib/messages/client/chat');
 
 
-function client_packet_reader(Packet){
-    let packet_type = Packet.read_uint8;
+function client_packet_reader(packet){
+    let packet_type = packet.read_uint8;
 
     if (packet_type == CHAT_MESSAGE){
-        let username = Packet.read_string();
-        let message = Packet.read_string();
+        let username = packet.read_string();
+        let message = packet.read_string();
         console.log(username+": "+message);
     }
 }
