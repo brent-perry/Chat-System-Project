241bcaa1e47f9ab53545b31974d28c165c2e6929
JulianForeman <71847719+JulianForeman@users.noreply.github.com>
2021-02-25 09:50:04 -0800

    Unit Testing (#22)
    * Unit Testing
    
    Created continuous integration for unit tests.
    Added mocha, chai and nyc to package.json dev dependencies.
    Wrote unit tests for core packet features.
    
    * Update main.js

---
+++ test/server-authentication.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const auth = require('../lib/messages/server/authentication');
+const assert = require('chai').assert;
+
+describe('create_auth_request() function', function(){
+  it('Should return a packet with the corresponding uint8 telling what kind of packet it is and an ID that is passed in as a parameter', function(){
+    let packet = auth.create_auth_request(1000);
+    packet.reset();
+    assert.equal(5, packet.length());
+    assert.equal(auth.AUTHENTICATE_REQUEST, packet.read_uint8());
+    assert.equal(1000, packet.read_uint32());
+  });
+});
+
+describe('create_auth_fail() function', function(){
+  it('Should return a packet with the corresponding uint8 telling what kind of packet it is and another uint8 saying that the request failed', function(){
+    let packet = auth.create_auth_fail();
+    packet.reset();
+    assert.equal(2, packet.length());
+    assert.equal(auth.AUTHENITCATE_STATUS, packet.read_uint8());
+    assert.equal(auth.AUTHENITCATE_STATUS_FAIL, packet.read_uint8());
+  });
+});
+
+describe('create_auth_okay() function', function(){
+  it('Should return a packet with the corresponding uint8 telling what kind of packet it is and another uint8 saying that the request was okay', function(){
+    let packet = auth.create_auth_okay();
+    packet.reset();
+    assert.equal(2, packet.length());
+    assert.equal(auth.AUTHENITCATE_STATUS, packet.read_uint8());
+    assert.equal(auth.AUTHENITCATE_STATUS_OKAY, packet.read_uint8());
+  });
+});
