7cee3b853af16b8ea8decc320e676ac9c33e144e
brent-perry <53350346+brent-perry@users.noreply.github.com>
2021-03-03 11:15:49 -0800

    updates

--- client/src/js/theme.js
+++ client/src/js/theme.js
@@ -2,7 +2,7 @@
 
 export function enableThemes(){
     let themeButtons = document.querySelectorAll('#themesContainer .themes');
-    themeButtons.forEach(button => button.addEventListener('click',clickThemeButton))
+    themeButtons.forEach(button => button.addEventListener('click', clickThemeButton))
 }
 
 let currentTheme = 'default';
@@ -15,4 +15,14 @@ function clickThemeButton(){
     window.document.body.classList.add(selectedTheme);
     currentTheme = selectedTheme;
 };
+
+let themesButton = document.getElementById("themesDropdownButton");
+let themeContent = document.getElementById("themesContainer");
+themesButton.addEventListener("click", event =>{
+    if(themeContent.classList.contains("show"))
+        themeContent.classList.remove("show");
+    else
+        themeContent.classList.add("show");
+})
+
 
