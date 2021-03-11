63ce5dcda42be2d72a1761fc3fd2f146f95739a6
trevor <50567522+iamtrevorm@users.noreply.github.com>
2021-03-11 10:51:05 -0800

    Add missing files

---
+++ util/buffer_strings.js
@@ -0,0 +1,22 @@
+//TODO: add unit tests
+// Easy way to write a test is to convert a readable string to its code points. See:
+// http://www.asciitable.com
+// 'hello' -> [104, 101, 108, 108, 111]
+
+function bufferToBinaryString(arrayBuffer){
+	let str = '';
+	let uint8Array = new Uint8Array(arrayBuffer);
+	for (let i = 0 ; i < arrayBuffer.byteLength ; ++i)
+		str += String.fromCharCode(uint8Array[i]);
+	return str;
+}
+
+function binaryStringToBuffer(str){
+	let arrayBuffer = new ArrayBuffer(str.length);
+	let uint8Array = new Uint8Array(arrayBuffer);
+	for (let i = 0 ; i < str.length ; ++i)
+		uint8Array[i] = str.charCodeAt(i);
+	return arrayBuffer;
+}
+
+module.exports = {bufferToBinaryString,binaryStringToBuffer};
