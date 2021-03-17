65d29473619787e1093b8c59b844893911084b58
JulianForeman <71847719+JulianForeman@users.noreply.github.com>
2021-03-17 11:14:57 -0700

    Guest Accounts & Channels (#30)
    * Merge
    
    * Guest Accounts. & Channels
    
    Created functions to set usernames and also functionality to join channels.
    
    Server only sends messages to people who are in the same channel as each other.
    
    * Commit package-lock.json
    
    * fixing test
    
    * clearing package-lock.json
    
    * Recreated package-lock.json
    
    * Updated test
    
    * Spelling corrections
    
    * Corrections
    
    * Formatting fixes

--- util/packetreader.js
+++ util/packetreader.js
@@ -3,19 +3,59 @@
 const { publisher } = require('../lib/redis');
 const {bufferToBinaryString,binaryStringToBuffer} = require('./buffer_strings');
 const Packet = require('../lib/packet');
-const { CHAT_MESSAGE } = require('../lib/messages/client/chat');
-
+const {CHAT_MESSAGE} = require('../lib/messages/client/chat');
+const {JOIN_CHANNEL} = require('../lib/messages/client/channel');
+const authObj = require('../lib/messages/client/authentication');
 
 function packet_reader(buffer, server, client){
   const packet = new Packet(buffer);
   const pId = packet.read_uint8();
-  switch (pId) {
+  switch (pId){
+    case JOIN_CHANNEL:
+      let channel = packet.read_string();
+      if (channel.length > 20){
+        throw new Error('Channel length exceeds 20 characters!');
+      }
+      console.log(`client joined channel: ${channel}`);
+      client.channel = channel;
+      break;
+
     case CHAT_MESSAGE:
+      if (!client.channel){
+        console.log("client tried to send message but has not joined a channel");
+        break; // TODO send error that client should join channel first
+      }
       let username = packet.read_string();
       let message = packet.read_string();
-      console.log(username + ": " + message);
+      let user_channel = client.channel;
       let binaryString = bufferToBinaryString(packet.buffer);
-      publisher.publish('chat',binaryString);
+      publisher.publish(`chat:${user_channel}`,binaryString);
+      break;
+
+    case authObj.AUTHENTICATE_RESPONSE:
+      let authUsername = packet.read_string();
+      let usernameTaken = false;
+      if (typeof username === 'string' && authUsername.length < 20){
+        server.clients.forEach((item, i) =>{
+          if (item.username == authUsername){
+            usernameTaken = true;
+          }
+        });
+      }
+      else{
+        let outgoingPacket = authObj.create_auth_fail(authObj.AUTHENTICATE_STATUS_FORMAT_ERROR);
+        client.send(outgoingPacket.buffer);
+        break;
+      }
+      if (usernameTaken){
+        let outgoingPacket = authObj.create_auth_fail(authObj.AUTHENTICATE_STATUS_NAME_TAKEN);
+        client.send(outgoingPacket.buffer);
+        break;
+      }
+      let outgoingPacket = authObj.create_auth_okay();
+      client.username = authUsername;
+      client.send(outgoingPacket.buffer);
+      break;
   }
 }
 module.exports = { packet_reader };
