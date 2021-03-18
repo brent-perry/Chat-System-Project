a3ccbf476149d332d312e587bc963c01de0a3ba4
brent-perry <53350346+brent-perry@users.noreply.github.com>
2021-03-18 10:02:52 -0700

    Brent/channel styles (#32)
    * updated channels
    
    * added channel switch logic
    
    * update indents

--- client/src/main.js
+++ client/src/main.js
@@ -2,21 +2,32 @@
 
 import {CHAT_MESSAGE} from '../../lib/messages/client/chat';
 import {enableThemes} from './js/theme';
+import {channelSelector} from './js/channel';
 import {sendChat,chat_socket} from './socket';
 
 enableThemes();
+channelSelector();
 
 var msgInfo = document.getElementsByClassName('mesgChat')[0];
 var sendMesg = document.getElementById('sendText');
 var convoBox = document.getElementById("conversationBoxWrapper");
+var chatForm = document.querySelector("form.chat");
 var username = 'Julian';
 
-sendMesg.addEventListener('click', function(){
+function submitChat(event){
+  event.preventDefault();
+  if (!msgInfo.value.length)
+    return;
   chat_socket.sendChat(username, msgInfo.value);
   msgInfo.value = '';
-});
+  }
+
+sendMesg.addEventListener('click', submitChat);
+chatForm.addEventListener('submit', submitChat);
 
 chat_socket.on("error",console.error);
+
+chat_socket.on("open",() => chat_socket.joinChannel('lobby'));
 
 chat_socket.on(CHAT_MESSAGE,function(packetObj){
   var conversationBox = document.createElement("DIV");
